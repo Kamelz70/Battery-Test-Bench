8847d806653f734ae0571ff857b92a705bed19b9
Kamelz70 <mohamed.kamel.m73@gmail.com>
2025-06-15 15:02:54 +0200

    fixed all parameters

--- BatteryTB_BTMC/circuit_control.cpp
+++ BatteryTB_BTMC/circuit_control.cpp
@@ -4,30 +4,17 @@
 #include "custom_types.h"
 
 enum CIRCUITMODE CircuitMode = CIRCUITOFF;
-int ChargingStopTime = DEFAULT_STOPTIME;
+int ChargingPulseOnTime = DEFAULT_PULSE_ON_TIME_S;
+int ChargingPulseOffTime = DEFAULT_PULSE_OFF_TIME_S;
 int ChargingCurrent = DEFAULT_CURRENT;
-int DischargingStopTime = DEFAULT_STOPTIME;
+int DischargingPulseOnTime = DEFAULT_PULSE_ON_TIME_S;
+int DischargingPulseOffTime = DEFAULT_PULSE_OFF_TIME_S;
 int DischargingCurrent = DEFAULT_CURRENT;
 
 void parseControlString(String controlString)
 {
 
-    //   switch (controlString[0])
-    //   {
-    //   case 'c':
-    //     CircuitMode = CIRCUITCHARGING;
-    //     break;
-    //   case 'd':
-    //     CircuitMode = CIRCUITDISCHARGING;
-    //     break;
-    //   case 'o':
-    //     CircuitMode = CIRCUITOFF;
-    //     return;
-    //     break;
-    //     default:
-    //     return;
-    //   }
-    if (controlString[0] == 'o'&&controlString.length()==1)
+    if (controlString[0] == 'o' && controlString.length() == 1)
     {
         CircuitMode = CIRCUITOFF;
         return;
@@ -41,17 +28,19 @@ void parseControlString(String controlString)
     {
         CircuitMode = CIRCUITCHARGING;
         ChargingCurrent = splitter.getItemAtIndex(1).toFloat();
-        ChargingStopTime = splitter.getItemAtIndex(2).toFloat();
+        ChargingPulseOnTime = splitter.getItemAtIndex(2).toFloat();
+        ChargingPulseOffTime = splitter.getItemAtIndex(3).toFloat();
         Serial.println("ChargingCurrent:" + String(ChargingCurrent));
-        Serial.println("ChargingStopTime:" + String(ChargingStopTime));
+        Serial.println("ChargingPulseOnTime:" + String(ChargingPulseOnTime));
     }
     else if (controlString[0] == 'd')
     {
         CircuitMode = CIRCUITDISCHARGING;
         DischargingCurrent = splitter.getItemAtIndex(1).toFloat();
-        DischargingStopTime = splitter.getItemAtIndex(2).toFloat();
+        DischargingPulseOnTime = splitter.getItemAtIndex(2).toFloat();
+        DischargingPulseOffTime = splitter.getItemAtIndex(3).toFloat();
         Serial.println("DischargingCurrent:" + String(DischargingCurrent));
-        Serial.println("DischargingStopTime:" + String(DischargingStopTime));
+        Serial.println("DischargingPulseOnTime:" + String(DischargingPulseOnTime));
     }
     else
     {
@@ -65,15 +54,18 @@ String getCircuitMode()
 }
 String getBoundarySettingsString()
 {
-    return String((int)MAX_VOLTAGE, 2) + "," + // 1
-           String(MIN_VOLTAGE, 2) + "," +      // 2
-           String(MAX_CURRENT) + "," +         // 3
-           String(MIN_CURRENT, 2) + "," +      // 4
-           String(MAX_TEMPERATURE, 2) + "," +  // 5
-           String(MIN_TEMPERATURE, 2) + "," +  // 6
-           String(MAX_STOPTIME) + "," +        // 7
-           String(MIN_STOPTIME, 2) + "," +     // 8
-           String(DEFAULT_STOPTIME) + "," +    // 9
-           String(DEFAULT_VOLTAGE, 2) + "," +  // 10
-           String(DEFAULT_CURRENT, 2);         // 11
+    return String(MAX_VOLTAGE, 2) + "," +    // 1
+           String(MIN_VOLTAGE, 2) + "," +         // 2
+           String(MAX_CURRENT) + "," +            // 3
+           String(MIN_CURRENT, 2) + "," +         // 4
+           String(MAX_TEMPERATURE, 2) + "," +     // 5
+           String(MIN_TEMPERATURE, 2) + "," +     // 6
+           String(MAX_PULSE_ON_TIME_S) + "," +    // 7
+           String(MIN_PULSE_ON_TIME_S) + "," + // 8
+           String(MAX_PULSE_OFF_TIME_S) + "," +    // 9
+           String(MIN_PULSE_OFF_TIME_S) + "," + // 10
+           String(DEFAULT_PULSE_ON_TIME_S) + "," +       // 11
+           String(DEFAULT_PULSE_OFF_TIME_S) + "," +       // 12
+           String(DEFAULT_VOLTAGE, 2) + "," +     // 13
+           String(DEFAULT_CURRENT, 2);            // 14
 }
